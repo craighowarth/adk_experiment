6d30ef6b29667a0aea4a40b5a77e90b7a3fad736
Jon Lazar <jon.lazar@gmail.com>
2020-09-02 13:43:15 -0400

    add example performance metric test

---
+++ SampleTests/SamplePerformanceMetricTests.swift
@@ -0,0 +1,22 @@
+//
+//  SamplePerformanceMetricTests.swift
+//  SampleTests
+//
+//  Created by Jonathan Lazar on 9/2/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+@testable import Sample
+import XCTest
+
+class SamplePerformanceMetricTests: XCTestCase {
+
+  func testTexturePerformance() throws {
+    let vc = ViewController()
+    measureMetrics([.wallClockTime], automaticallyStartMeasuring: false, for: {
+      startMeasuring()
+      vc.view.layoutIfNeeded()
+      stopMeasuring()
+    })
+  }
+}
